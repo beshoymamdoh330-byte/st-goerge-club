15ace77fb29bf3edba2f98d8e89b8eb03128b508
bmamdoh199-hub <bmamdoh199@gmail.com>
2026-08-13 01:03:53 +0300

    ya rab

--- app/pages/RegisterPage.tsx
+++ app/pages/RegisterPage.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
 import Login from '../components/Login'
-import SignUp from "../components/SignUp"
+// import SignUp from "../components/SignUp"
 import { useFormContext, useThemeContext } from '../assets/contexts'
 import Header from '../components/Header'
 export default function RegisterPage() {
@@ -13,9 +13,10 @@ return (
     <>
         <Header/>
         <main className={` bg-fixed w-full min-h-screen py-5 pt-25  md:px-10 ${theme==="light"?"light-mode":"dark-mode"} flex items-center justify-center`}>
-            {
+            {/* {
                 form === "login" ? <Login/> : <SignUp/>
-            }
+            } */}
+            <Login/>
         </main>
     
     </>
