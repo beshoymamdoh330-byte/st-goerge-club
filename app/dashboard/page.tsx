62f6a3fb898bdf74cffecc298abeb1b2ce6f50a1
bmamdoh199-hub <bmamdoh199@gmail.com>
2026-08-14 03:49:19 +0300

    fgrfgr

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import DashbordPage from '../pages/DashbordPage'
-export default function Dashboard() {
-  return (
-    <div>
-      <DashbordPage/>
-    </div>
-  )
+// import React from 'react'
+// import DashbordPage from '../pages/DashbordPage'
+// export default function Dashboard() {
+//   return (
+//     <div>
+//       <DashbordPage/>
+//     </div>
+//   )
+// }
+
+"use client"
+export const dynamic = 'force-dynamic'
+import DashbordPage from '../pages/DashbordPage' // تأكد من صحة مسار الملف بالنسبة لمشروعك
+
+export default function Page() {
+    return <DashbordPage />
 }
--- app/pages/DashbordPage.tsx
+++ app/pages/DashbordPage.tsx
@@ -1,4 +1,28 @@
-// "use client"
+// // "use client"
+// import React from 'react'
+// import DashboardContent from '../components/DashboardContent'
+// import DashboardMembers from '../components/DashboardMembers'
+// import DashboardSubs from '../components/DashboardSubs'
+// import { useThemeContext } from '../assets/contexts'
+
+// export default function DashbordPage() {
+//     const { theme } = useThemeContext()
+
+//     return (
+//         <main className={`${theme === "light" ? "light-mode" : "dark-mode"} w-full bg-fixed p-5 pt-25 min-h-screen`}>
+//             {/* محتوى الداشبورد الرئيسي */}
+//             <DashboardContent />
+
+//             {/* قسم الأعضاء والاشتراكات */}
+//             <section className='grid grid-cols-1 items-start md:grid-cols-2 gap-2.5 mt-5'>
+//                 <DashboardMembers />
+//                 <DashboardSubs />
+//             </section>
+//         </main>
+//     )
+// }
+
+"use client"
 import React from 'react'
 import DashboardContent from '../components/DashboardContent'
 import DashboardMembers from '../components/DashboardMembers'
@@ -10,10 +34,7 @@ export default function DashbordPage() {
 
     return (
         <main className={`${theme === "light" ? "light-mode" : "dark-mode"} w-full bg-fixed p-5 pt-25 min-h-screen`}>
-            {/* محتوى الداشبورد الرئيسي */}
             <DashboardContent />
-
-            {/* قسم الأعضاء والاشتراكات */}
             <section className='grid grid-cols-1 items-start md:grid-cols-2 gap-2.5 mt-5'>
                 <DashboardMembers />
                 <DashboardSubs />
